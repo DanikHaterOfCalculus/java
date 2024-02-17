61b9f3d1ca44e1cd83ece75a217c78ec893a0693
DanikHaterOfCalculus <turzhanov.danial@gmail.com>
2024-02-17 20:51:25 +0600

    car class getter update

--- untitled/src/Car.java
+++ untitled/src/Car.java
@@ -1,16 +1,26 @@
-class Car {
-    private String marka;
+public class Car {
+    private String brand;
     private int year;
-    private int probeg;
+    private int mileage;
 
-    public Car(String marka, int year, int probeg) {
-        this.marka=marka;
-        this.year=year;
-        this.probeg=probeg;
+    public Car(String brand, int year, int mileage) {
+        this.brand = brand;
+        this.year = year;
+        this.mileage = mileage;
     }
-    public void displaycar() {
-        System.out.println("marka " + marka);
-        System.out.println("year " + year);
-        System.out.println("probeg" + probeg);
+
+    public void displayCarList() {
+        System.out.println("Car: " + brand);
+        System.out.println("Year: " + year);
+        System.out.println("Probeg: " + mileage);
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public int getYear() {
+        return year;
+    }
+    public int getProbeg() {
+        return mileage;
     }
 }
