5cc33e1ac520f660e3f1fef6b5dcbf15a484e606
DanikHaterOfCalculus <turzhanov.danial@gmail.com>
2024-02-17 14:49:36 +0600

    Car add and rent

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         Car car = new Car("Toyota", 2020, 5000);
         Rent rent=new Rent(1, "Era", "Toyota", 1);
-        car.displaycar();
-        rent.displayRentDetails();
+        while (true){
+            System.out.println("\nCar rental management system:");
+            System.out.println("1. Add a new car");
+            System.out.println("2. Rent a car");
+            int choice=scanner.nextInt();
+            scanner.nextLine();
+            switch(choice){
+                case 1: car.displaycar();
+                case 2: rent.displayRentDetails();
+            }
+        }
+
+
+
+
+
+
 
 
     }
