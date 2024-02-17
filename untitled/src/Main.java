5844aa03c70fc5f68a169f491054ebda1049a622
DanikHaterOfCalculus <turzhanov.danial@gmail.com>
2024-02-17 14:36:23 +0600

    Rent class adding

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+
         Car car = new Car("Toyota", 2020, 5000);
+        Rent rent=new Rent(1, "Era", "Toyota", 1);
         car.displaycar();
+        rent.displayRentDetails();
+
+
     }
-}  
+}
